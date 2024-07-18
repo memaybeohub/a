ec001fa3bb1896571502f0276af9974e3f45b2b4
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2024-07-18 19:10:30 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,4 +7,4 @@ def hello():
     return "Xin chào từ Render.com!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000) # Cổng 10000 được Render.com yêu cầu
+    app.run(host='0.0.0.0', port=6041, debug=True) # Cổng 10000 được Render.com yêu cầu

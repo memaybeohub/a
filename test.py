f24b43298a674feb230a3c913d22157f8fa70ff7
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2024-07-18 19:10:03 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Xin chào! Host của bạn đang hoạt động với Flask!"
+    return "Xin chào từ Render.com!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000) # Cổng 10000 được Render.com yêu cầu
